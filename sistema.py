e9a66d9db6d9d3e7783b95d7f6c85ee64cb805da
marmedempreendimentosltda-droid <marmedempreendimentosltda@gmail.com>
2026-06-20 13:26:09 -0300

    Create sistema.py

---
+++ sistema.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import sqlite3
+import hashlib
+import os
+from datetime import datetime
+
+st.set_page_config(page_title="MARMED - Gestão em Saúde Pública de Luminárias-MG", layout="wide")
+...
