a7d85eeeca28329b450423233115d18210fc0789
marmedempreendimentosltda-droid <marmedempreendimentosltda@gmail.com>
2026-06-21 08:19:47 -0300

    Update sistema.py

--- sistema.py
+++ sistema.py
@@ -326,7 +326,7 @@ with st.expander('Editar Metricas'):
                 st.success('Metricas atualizadas!')
                 st.rerun()
 
-    st.markdown('<br>', unsafe_allow_html=True)
+st.markdown('<br>', unsafe_allow_html=True)
     st.markdown('<h3 style="color:#00d4ff;">Resumo Financeiro</h3>', unsafe_allow_html=True)
 
     conn = get_conn()
